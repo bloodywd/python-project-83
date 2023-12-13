31438ec6d9e1f2575d58f16d2b17c8591af870ce
bloodywd <bloodyforreal@gmail.com>
2023-12-14 00:44:50 +0300

    create DB for project

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
 from dotenv import load_dotenv
+import os
+import psycopg2
 
 
 load_dotenv()
+DATABASE_URL = os.getenv('DATABASE_URL')
+try:
+    conn = psycopg2.connect(DATABASE_URL)
+    print('Fine')
+except:
+    print('Can`t establish connection to database')
+
 app = Flask(__name__)
-
-
 @app.route('/')
 def get_main():
     return render_template(
