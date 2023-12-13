c06a5322fe517c884531addf41006ffc6b062a73
bloodywd <bloodyforreal@gmail.com>
2023-12-14 02:41:05 +0300

    created @url_post

--- Makefile
+++ Makefile
@@ -4,6 +4,7 @@ PORT ?= 8000
 install:
 	poetry install
 dev:
+	./dev.sh
 	poetry run flask --app page_analyzer:app --debug run
 start:
 	poetry run gunicorn -w 5 -b 0.0.0.0:$(PORT) page_analyzer:app
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,20 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect, flash, get_flashed_messages
 from dotenv import load_dotenv
 import os
 import psycopg2
+from validators import url as validate
+from datetime import datetime
 
 
 load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
-try:
-    conn = psycopg2.connect(DATABASE_URL)
-    print('Fine')
-except:
-    print('Can`t establish connection to database')
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+
 @app.route('/')
 def get_main():
+    messages = get_flashed_messages()
     return render_template(
-        'index.html'
+        'index.html',
+        messages=messages
+    )
+
+
+@app.post('/')
+def post_url():
+    url = request.form.get('url')
+    if not validate(url):
+        flash('Некорректный url', 'error'),
+    else:
+        current_datetime = datetime.now()
+        timestamp = current_datetime.strftime('%Y-%m-%d %H:%M:%S')
+        conn = psycopg2.connect(DATABASE_URL)
+        cur = conn.cursor()
+        cur.execute("INSERT INTO urls (name, created_at) VALUES (%s, %s)", (url, timestamp))
+        conn.commit()
+        cur.close()
+        flash('Успешно добавлено', 'success')
+
+    return redirect(
+        url_for('get_main')
     )
